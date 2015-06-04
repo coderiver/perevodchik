4a9b9905a8aacfded3a9253bb9b717963a991070
Валерий <kaktuz34@gmail.com>
2015-06-04 11:02:14 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,18 +1,61 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	// click
+	$(document).click(function() {
+		$(".js-select").removeClass("is-active");
+		$(".js-select-list").slideUp(100);
+	});
 
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
+	// filter
+	$('.js-filter').on('click', function() {
+		if ($(this).hasClass('is-active')) {
+			$(this).removeClass('is-active');
+		}
+		else {
+			$(this).addClass('is-active');
+		}
+	});
+
+	// select
+	$("body").on("click",".js-select",function(event) {
+		event.stopPropagation();
+	});
+	$("body").on("click",".js-select-text",function(event) {
+		var select = $(this).parents(".js-select");
+		if (select.hasClass("is-active")) {
+			$(".js-select").removeClass("is-active");
+			$(".js-select-list").slideUp(100);
+		}
+		else {
+			$(".js-select").removeClass("is-active");
+			$(".js-select-list").slideUp(100);
+			select.toggleClass("is-active").find(".js-select-list").slideToggle(100);
+		}
+		
+	});
+
+	$("body").on("click",".js-select-list li",function() {
+		var val = $(this).attr("data-val");
+		var text = $(this).text();
+		var select = $(this).parents(".js-select");
+		var selectList = $(this).parents(".js-select-list");
+		select.find(".js-select-text").text(text);
+		select.find("option").removeAttr("selected");
+		select.find('option[value="'+val+'"]').attr("selected", "selected");
+		selectList.find("li").removeClass("is-active");
+		$(this).addClass("is-active");
+		select.removeClass("is-active");
+		selectList.slideUp(100);
+		return false;
+	});
+
+	// height
+	function wHeight() {
+		if ($(window).height() > ($('.js-col2').height() + 97)) {
+			$('.js-out').addClass('is-full');
+		} else {
+			$('.js-out').removeClass('is-full');
+		}
+	} wHeight();
 
 });
